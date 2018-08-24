27e65077611de351a1fa914a79efa27959adfca6
huangjiaming@cootf.com <huangjiaming@cootf.com>
2018-08-24 17:01:48 +0800

    添加响应式菜单

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches)
+  );
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit () {
+
+  }
 }
